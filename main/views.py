744136446bbbc57991c6912b54bc69683d6728de
dilip gupta <dilip98914@gmail.com>
2019-02-20 10:25:15 +0530

    initial commit

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def homepage(request):
+	title='hello'
+	return render(request,
+					'main/home.html',
+					{'title':title})
+
+
