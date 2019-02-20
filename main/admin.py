4fc9cb3a383088e475985bef5cd7c77c24aaee6e
dilip gupta <dilip98914@gmail.com>
2019-02-20 10:41:34 +0530

    added templating

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tutorial
+from django.db import models
+
+admin.site.register(Tutorial)
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+
+
 
 # Create your models here.
+class Tutorial(models.Model):
+	title=models.CharField(max_length=200)
+	content=models.TextField()
+	published=models.DateTimeField('published',default=datetime.now)
