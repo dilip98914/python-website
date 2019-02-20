bf64c0e3b493aa1f43e45743412291462dadfb6b
dilip gupta <dilip98914@gmail.com>
2019-02-20 10:58:47 +0530

    added regsiter link

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-	path('',views.homepage,name='homepage')
+	path('',views.homepage,name='homepage'),
+	path('register/',views.register,name='register')
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import login,logout,authenticate
+from .models import Tutorial
 
 
 def homepage(request):
@@ -10,3 +13,25 @@ def homepage(request):
 					{'title':title})
 
 
+def register(request):
+	if request.method=='POST':
+		# crate form object
+		form=UserCreationForm(request.POST)
+		# check for validation
+		if form.is_valid():
+			# essentially saving or creating user
+			user=form.save()
+			username=form.cleaned_data.get('username')
+			# login with request and user just created
+			login(request,user)
+			return redirect('/')
+		else:
+			print('validation error')
+			return render(request,
+						'main/register.html',
+						{'form':form})
+
+	form=UserCreationForm()
+	return render(request,
+					'main/register.html',
+						{'form':form})
